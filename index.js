0a1c2d171c409c75ee42995079edb8b01e7ce6a3
SJC2021 <82860116+SJC2021@users.noreply.github.com>
2021-09-12 00:29:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express"),
+  morgan = require("morgan");
+
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.static("public"));
 
-let topMovies = [
+const bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+let movies = [
   {
     title: "Snatch",
     year: "2000",
@@ -64,7 +82,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.json(topBooks);
+  res.json(movies);
 });
 
 // listen for requests
