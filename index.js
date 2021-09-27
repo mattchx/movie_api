6c7ebf4b8f786e76d2ab4488772c8475b6520f1d
SJC2021 <82860116+SJC2021@users.noreply.github.com>
2021-09-26 22:04:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -275,6 +275,21 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.listen(8080, () => {
-  console.log("movie_api is listening on port 8080.");
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
